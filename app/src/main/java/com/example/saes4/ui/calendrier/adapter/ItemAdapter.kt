25fac7dd184c3a571895bee2c6e659b4a6900120
lehmann lucas <lucas.lehmann@edu.univ-fcomte.fr>
2023-03-08 11:37:50 +0100

    edit ItemAdapter.kt

--- app/src/main/java/com/example/saes4/ui/calendrier/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/saes4/ui/calendrier/adapter/ItemAdapter.kt
@@ -1,21 +1,38 @@
 package com.example.saes4.ui.calendrier.adapter
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.saes4.R
 import com.example.saes4.ui.calendrier.CalendrierFragment
 import com.example.saes4.ui.calendrier.model.CalendarItem
 
-class ItemAdapter(calendrierFragment: CalendrierFragment, calendarDataset: List<CalendarItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ItemAdapter(
+    private val calendrierFragment: CalendrierFragment,
+    private val calendarDataset: List<CalendarItem>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         TODO("Not yet implemented")
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.calendar_item, parent, false)
+
+        return CalendarViewHolder(adapterLayout)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return calendarDataset.size
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         TODO("Not yet implemented")
+        val item = calendarDataset[position]
+//        holder.itemCalendar
+
     }
 
+    class CalendarViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val itemCalendaar: TextView = view.findViewById(R.id.calendar_item)
+    }
 }
