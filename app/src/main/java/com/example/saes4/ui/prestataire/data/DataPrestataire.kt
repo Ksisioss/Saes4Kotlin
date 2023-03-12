87cb6526eab75959f764d120d7c011c58e817cea
Julien Merveaux <jmerveau@iut-bm.univ-fcomte.fr>
2023-03-12 17:52:02 +0100

    listePrestataire finish

--- app/src/main/java/com/example/saes4/ui/prestataire/data/DataPrestataire.kt
+++ app/src/main/java/com/example/saes4/ui/prestataire/data/DataPrestataire.kt
@@ -5,41 +5,56 @@ import com.example.saes4.ui.prestataire.model.Prestataire
 
 
 class DataPrestataire {
-
     fun loadPrestataire(): List<Prestataire> {
         return listOf<Prestataire>(
             Prestataire(
-                R.string.prenom1,
-                R.string.nom1,
-                R.drawable.ic_launcher_foreground,
-                R.string.nomStand1,
-                R.string.categorie1,
+                R.string.julien,
+                R.string.merveaux,
+                R.drawable.prestataire_1,
+                R.string.barquetteDeFredy,
+                R.string.spectacle,
                 R.string.presentatation1,
             ),
             Prestataire(
-                R.string.prenom1,
-                R.string.nom1,
-                R.drawable.ic_launcher_foreground,
-                R.string.nomStand1,
-                R.string.categorie1,
-                R.string.presentatation1,
+                R.string.lucas,
+                R.string.lehmann,
+                R.drawable.prestataire_2,
+                R.string.standMichelle,
+                R.string.food,
+                R.string.presentatation2,
             ),
             Prestataire(
-                R.string.prenom1,
-                R.string.nom1,
-                R.drawable.ic_launcher_foreground,
-                R.string.nomStand1,
-                R.string.categorie1,
-                R.string.presentatation1,
+                R.string.tibault,
+                R.string.lebreuil,
+                R.drawable.prestataire_3,
+                R.string.FondueMetalique,
+                R.string.activite,
+                R.string.presentatation3,
             ),
             Prestataire(
-                R.string.prenom1,
-                R.string.nom1,
-                R.drawable.ic_launcher_foreground,
-                R.string.nomStand1,
-                R.string.categorie1,
-                R.string.presentatation1,
+                R.string.george,
+                R.string.nelkson,
+                R.drawable.prestataire_4,
+                R.string.regimeDesVichy,
+                R.string.food,
+                R.string.presentatation4,
             ),
         )
     }
+
+    fun getPrestataireActivite(): List<Prestataire> {
+        val prestataires = loadPrestataire()
+        return prestataires.filter { it.category == R.string.activite }
+    }
+
+    fun getPrestataireFood(): List<Prestataire> {
+        val prestataires = loadPrestataire()
+        return prestataires.filter { it.category == R.string.food }
+    }
+
+    fun getPrestataireSpectacle(): List<Prestataire> {
+        val prestataires = loadPrestataire()
+        return prestataires.filter { it.category == R.string.spectacle }
+    }
+
 }
