f26bc53250c376c1cc152544b11bc46b247f1b47
lehmann lucas <lucas.lehmann@edu.univ-fcomte.fr>
2023-03-22 17:26:35 +0100

    Interaction with map functional

--- app/src/main/java/com/example/saes4/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/saes4/ui/map/MapFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import com.example.saes4.R
 import com.example.saes4.databinding.FragmentMapBinding
 
 class MapFragment : Fragment() {
@@ -33,6 +37,43 @@ class MapFragment : Fragment() {
         }
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val marker1: ImageView = view.findViewById(R.id.marker1);
+        marker1.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.fragment_prestaire_profil, bundleOf("presta" to "1"))
+        }
+        val marker2: ImageView = view.findViewById(R.id.marker2);
+        marker2.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.fragment_prestaire_profil, bundleOf("presta" to "2"))
+        }
+        val marker3: ImageView = view.findViewById(R.id.marker3);
+        marker3.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.fragment_prestaire_profil, bundleOf("presta" to "3"))
+        }
+        val marker4: ImageView = view.findViewById(R.id.marker4);
+        marker4.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.fragment_prestaire_profil, bundleOf("presta" to "4"))
+        }
+        val marker5: ImageView = view.findViewById(R.id.marker5);
+        marker5.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.fragment_prestaire_profil, bundleOf("presta" to "5"))
+        }
+        val marker6: ImageView = view.findViewById(R.id.marker6);
+        marker6.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.fragment_prestaire_profil, bundleOf("presta" to "6"))
+        }
+        val marker7: ImageView = view.findViewById(R.id.marker7);
+        marker7.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.fragment_prestaire_profil, bundleOf("presta" to "7"))
+        }
+        val marker8: ImageView = view.findViewById(R.id.marker8);
+        marker8.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.fragment_prestaire_profil, bundleOf("presta" to "0"))
+        }
     }
 
     override fun onDestroyView() {
